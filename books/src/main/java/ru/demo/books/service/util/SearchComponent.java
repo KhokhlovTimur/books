e5c6326dbf115bbc7f638abdc54a4874fdcb42a0
KhokhlovTimur <t.kh25@mail.ru>
2024-06-18 18:46:19 +0300

    refactor: remove package

--- books/src/main/java/ru/demo/books/service/util/SearchComponent.java
+++
@@ -1,9 +0,0 @@
-package ru.demo.books.service.util;
-
-import ru.demo.books.dto.book.SearchBooksRequest;
-
-public interface SearchComponent {
-
-    public void search(SearchBooksRequest req);
-
-}
